c94a617cce0d7880553ef9f1a5fc74e2be6e3b0b
Gekki <antoinemalaval@aol.com>
2017-11-14 11:21:04 +0100

    2eme modif
    ajout d'un commentaire2eme modif
    
    ajout d'un commentaire

--- src/Modele.java
+++ src/Modele.java
@@ -10,7 +10,7 @@ import com.mysql.jdbc.PreparedStatement;
 
 
 public class Modele {
-
+	//coucou madame
 	private static Connection connexion ;
 	private static Statement state;
 	private static ResultSet rs, rs2;
